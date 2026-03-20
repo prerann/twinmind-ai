8aa6739a60f79aff92011e674a342ca38679212b
preran <cvnprreran1239@gmail.com>
2026-03-20 21:25:10 +0530

    moved api key to config

--- models/llm.py
+++ models/llm.py
@@ -1,6 +1,7 @@
 from groq import Groq
+from config.config import GROQ_API_KEY
 
-client = Groq(api_key=st.secrets["GROQ_API_KEYS"])
+client = Groq(api_key=GROQ_API_KEY)
 
 def generate_response(messages):
     try:
